6c365eaabc998d4efc4d7a5a32fdb38c2d529acb
ChachezMic <chachezmic@gmail.com>
2024-05-03 16:37:09 -0700

    not sure

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-# Creates a file with content
-file { '/tmp/holberton':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+# create a file
+
+file {'/tmp/school':
+ensure  => 'present',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
