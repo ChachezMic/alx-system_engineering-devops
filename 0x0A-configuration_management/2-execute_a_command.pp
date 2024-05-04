3d3449d938fb43ddeeb9528ddaeb408bcaa38947
ChachezMic <chachezmic@gmail.com>
2024-05-03 19:08:55 -0700

    nomistakes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Kills a process
+
+exec {'killmenow':
+  command  => 'pkill -f killmenow',
+  provider => 'shell',
 }
