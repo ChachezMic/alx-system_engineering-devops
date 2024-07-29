90a7b6b1649734597f3194e96feb171358246136
ChachezMic <chachezmic@gmail.com>
2024-07-29 08:14:52 -0700

    almosthom...

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,3 +13,18 @@ package { 'nginx':
 file {'/var/www/html/index.html':
 	content => 'Hello World!'
 }
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
